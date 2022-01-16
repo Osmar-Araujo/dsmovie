003b69ed4eaf98bef85ea0fdf189a60886975234
Osmar-Araujo <araujooa@gmail.com>
2022-01-16 13:56:57 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing(){
         empty: true
     });
 
-    useEffect(() => { axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+    useEffect(() => { axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
     .then(response => {
         const data = response.data as MoviePage;
         setPage(data);
